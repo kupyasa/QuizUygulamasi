2761175b6546d4f94a9bbcbf4ea1db6ba6f357c7
Yakup Yaşa <yakupyasa1@gmail.com>
2022-04-19 00:26:47 +0300

    Question migration,factory,seeder hazıralandı.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             QuizSeeder::class,
-
+            QuestionSeeder::class
         ]);
     }
 }
